bd648f3594ee777c72e2fc697c6c3be76482408a
Howie315 <nguyen.howie2010@gmail.com>
2024-05-25 18:40:36 -0700

    updated packages

--- features/Login/presentation/Login.tsx
+++ features/Login/presentation/Login.tsx
@@ -1,69 +1,78 @@
 import loginSheet from "./LoginSheet";
 import React, { useState } from "react";
 import {
-	View,
-	Text,
-	TextInput,
-	Button,
-	TouchableOpacity,
-	Image,
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+// import Logo from "../../../assets/logo.png";
 import Logo from "../../../assets/logo.png";
 import background1 from "../../../assets/login-background1.png";
 import background2 from "../../../assets/login-background2.png";
 
 const Login = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-	const handleLogin = () => {
-		console.log("Login attempt with:", username, password);
-		// Implement your login logic here
-	};
+  console.log("Logo path:", Logo);
+  console.log("Background1 path:", background1);
+  console.log("Background2 path:", background2);
 
-	return (
-		<LinearGradient
-			colors={["#000000", "#3B20A8"]} // Gradient colors
-			style={loginSheet.container}
-			start={{ x: 0, y: 0 }} // Start of gradient
-			end={{ x: 0, y: 1 }} // End of gradient
-		>
-			<Image
-				source={background1}
-				resizeMode="contain" // or 'cover' depending on your need
-				style={loginSheet.backgroundImage}
-			/>
-			<Image
-				source={background2}
-				resizeMode="contain" // or 'cover' depending on your need
-				style={loginSheet.backgroundImage2}
-			/>
-			<Text style={loginSheet.title}>Welcome Back!</Text>
-			<View style={loginSheet.modalContainer}>
-				<TextInput
-					style={loginSheet.input}
-					placeholder="Username.."
-					placeholderTextColor="#E0E0E0"
-					value={username}
-					onChangeText={setUsername}
-				/>
-				<TextInput
-					style={loginSheet.input}
-					placeholder="Password.."
-					placeholderTextColor="#E0E0E0"
-					value={password}
-					secureTextEntry={true}
-					onChangeText={setPassword}
-				/>
-				<Text style={loginSheet.forgetPassword}>Forgot Password?</Text>
-				<Image source={Logo} style={{}} />
-				<TouchableOpacity style={loginSheet.button} onPress={handleLogin}>
-					<Text style={loginSheet.buttonText}>Sign in</Text>
-				</TouchableOpacity>
-			</View>
-		</LinearGradient>
-	);
+  const handleLogin = () => {
+    console.log("Login attempt with:", username, password);
+    // Implement your login logic here
+  };
+
+  return (
+    <LinearGradient
+      colors={["#000000", "#3B20A8"]} // Gradient colors
+      style={loginSheet.container}
+      start={{ x: 0, y: 0 }} // Start of gradient
+      end={{ x: 0, y: 1 }} // End of gradient
+    >
+      <Image
+        source={background1}
+        resizeMode="contain" // or 'cover' depending on your need
+        style={loginSheet.backgroundImage}
+      />
+      <Image
+        source={background2}
+        resizeMode="contain" // or 'cover' depending on your need
+        style={loginSheet.backgroundImage2}
+      />
+      <Text style={loginSheet.title}>Welcome Back!</Text>
+      <View style={loginSheet.modalContainer}>
+        <TextInput
+          style={loginSheet.input}
+          placeholder="Username.."
+          placeholderTextColor="#E0E0E0"
+          value={username}
+          onChangeText={setUsername}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={loginSheet.input}
+          placeholder="Password.."
+          placeholderTextColor="#E0E0E0"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={loginSheet.forgetPassword}>Forgot Password?</Text>
+        <Image source={Logo} style={{}} />
+        <TouchableOpacity style={loginSheet.button} onPress={handleLogin}>
+          <Text style={loginSheet.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
 };
 
 export default Login;
--- features/Login/presentation/LoginSheet.ts
+++ features/Login/presentation/LoginSheet.ts
@@ -1,80 +1,80 @@
 import { StyleSheet } from "react-native";
 
 const loginSheet = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
+  container: {
+    flex: 1,
+    justifyContent: "center",
 
-		width: "100%",
-	},
-	title: {
-		fontSize: 50,
-		marginBottom: 20,
-		color: "#fff",
-		fontFamily: "Kodchasan_Bold",
-		width: "60%",
-		paddingLeft: 30,
-	},
-	backgroundImage: {
-		position: "absolute",
-		top: 0,
-		right: 0,
-		zIndex: 1, // Lower zIndex means it will be 'under' the text
-	},
-	backgroundImage2: {
-		position: "absolute",
-		bottom: -400,
-		right: 100,
-		zIndex: 1, // Lower zIndex means it will be 'under' the text
-	},
-	modalContainer: {
-		shadowColor: "#000",
-		shadowOffset: { width: 0, height: 4 },
-		shadowOpacity: 0.25,
-		shadowRadius: 4,
-		elevation: 4, // This is used for Android
-		backgroundColor: "#00000040", // Semi-transparent black background
-		padding: 20,
-		paddingTop: 30,
-		borderRadius: 30, // Rounded corners
-		alignItems: "center",
-		justifyContent: "center",
-		marginHorizontal: 30, // Adds horizontal margin
-		marginVertical: 50, // Adds vertical margin, adjust as needed
-	},
+    width: "100%",
+  },
+  title: {
+    fontSize: 50,
+    marginBottom: 20,
+    color: "#fff",
+    fontFamily: "Kodchasan_Bold",
+    width: "60%",
+    paddingLeft: 30,
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    zIndex: 1, // Lower zIndex means it will be 'under' the text
+  },
+  backgroundImage2: {
+    position: "absolute",
+    bottom: -400,
+    right: 100,
+    zIndex: 1, // Lower zIndex means it will be 'under' the text
+  },
+  modalContainer: {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 4, // This is used for Android
+    backgroundColor: "#00000040", // Semi-transparent black background
+    padding: 20,
+    paddingTop: 30,
+    borderRadius: 30, // Rounded corners
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 30, // Adds horizontal margin
+    marginVertical: 50, // Adds vertical margin, adjust as needed
+  },
 
-	input: {
-		width: 300,
-		height: 50,
-		padding: 30,
-		marginBottom: 10,
-		borderWidth: 1,
-		borderRadius: 30,
-		borderColor: "#fff",
-		fontSize: 18,
-		backgroundColor: "rgba(255, 255, 255, 0.2)",
-		color: "#E0E0E0",
-		fontFamily: "Kodchasan_Regular",
-	},
-	forgetPassword: {
-		fontFamily: "Kodchasan_Bold",
-		color: "#E0E0E0",
-		fontSize: 18,
-	},
-	button: {
-		backgroundColor: "#7339EB",
-		borderRadius: 30,
-		width: 180,
-		height: 50,
-	},
-	buttonText: {
-		marginTop: 8,
-		color: "#fff",
-		fontFamily: "Kodchasan_Bold",
-		textAlign: "center",
-		justifyContent: "center",
-		fontSize: 24,
-	},
+  input: {
+    width: 300,
+    height: 50,
+    padding: 30,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: "#fff",
+    fontSize: 18,
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    color: "#E0E0E0",
+    fontFamily: "Kodchasan_Regular",
+  },
+  forgetPassword: {
+    fontFamily: "Kodchasan_Bold",
+    color: "#E0E0E0",
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: "#7339EB",
+    borderRadius: 30,
+    width: 180,
+    height: 50,
+  },
+  buttonText: {
+    marginTop: 8,
+    color: "#fff",
+    fontFamily: "Kodchasan_Bold",
+    textAlign: "center",
+    justifyContent: "center",
+    fontSize: 24,
+  },
 });
 
 export default loginSheet;
